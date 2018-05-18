d61cb4cb49afde0a3e55d6a2a6e6421e590d54b3
Baptiste Foucher <32940266+baptistefoucher@users.noreply.github.com>
2018-05-18 16:27:34 +0200

    added Frontend

---
+++ Frontend/demo/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NavbarComponent } from './../../../src/navbars/navbar.component';
+import { NavbarService } from './../../../src/navbars/navbar.service';
+import { LogoComponent } from './../../../src/navbars/logo.component';
+import { LinksComponent } from './../../../src/navbars/links.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from '../../../src';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]),
+    MDBBootstrapModule.forRoot(),
+    AngularFontAwesomeModule
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [NavbarService],
+  bootstrap: [AppComponent],
+  exports: [ NavbarComponent, LinksComponent, LogoComponent],
+})
+export class AppModule {
+}
