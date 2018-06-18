fdd762bd44fc2519b9d86c132e713a76182820b5
cgeindreau <carine.geindreau@libre-logic.fr>
2018-06-18 20:00:30 +0200

    Frontend : ajout des services et models. Frontend : maj de la config.  Backend : MAJ des modifications FK

---
+++ Frontend/src/app/models/stagiaire.ts
@@ -0,0 +1,13 @@
+// Model de l'objet : Stagiaire
+
+
+export class Stagiaire {
+    CodeStagiaire: number;
+    Civilite:string;
+    Nom: string;
+    Prenom: string;
+
+    constructor() {
+    }
+
+}
