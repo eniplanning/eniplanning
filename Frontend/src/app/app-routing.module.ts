f8f55ad9727aa6da5e409b9faabd2d8233e77240
Baptiste Foucher <32940266+baptistefoucher@users.noreply.github.com>
2018-06-20 09:22:10 +0200

    Updated menu

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Lazy loading. Real routes are defined in each module (modulename-routing.module)
 const routes: Routes = [
 	{
 		path: 'planning',
 		loadChildren: './planning/planning.module#PlanningModule'
+	},
+	{
+		path: 'modeles',
+		loadChildren: './modeles/modeles.module#ModelesModule'
+	},
+	{
+		path: 'mon-compte',
+		loadChildren: './mon-compte/mon-compte.module#MonCompteModule'
+	},
+	{
+		path: 'login',
+		loadChildren: './login/login.module#LoginModule'
 	},
 	{
 	  path: '',
