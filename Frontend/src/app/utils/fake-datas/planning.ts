4542fd32d8fa38321c9bd6a6d8850a43a67cbeb5
cgeindreau <carine.geindreau@libre-logic.fr>
2018-06-20 14:49:07 +0200

    ajout des fake-datas Front

---
+++ Frontend/src/app/utils/fake-datas/planning.ts
@@ -0,0 +1,6 @@
+import { Planning } from '../models/planning';
+
+export const PLANNINGS: Planning[] = [ 
+  { planning_id: 11, label: 'Faux planning en dur 1', status_selected: false  },
+  { planning_id: 13, label: 'Faux planning en dur 2', status_selected: false },
+];
