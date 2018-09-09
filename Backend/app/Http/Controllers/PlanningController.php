6e43169861d6d9f014ca0f985d52c8997e881e56
valentin <belaud.valentin@gmail.com>
2018-09-10 00:56:39 +0200

    Ajout d'une seed pour créer l'utilisateur Admin

--- Backend/app/Http/Controllers/PlanningController.php
+++ Backend/app/Http/Controllers/PlanningController.php
@@ -76,7 +76,7 @@ class PlanningController extends Controller
     {
         Log::info('=> ' . get_class($this) . ' :: ' . __FUNCTION__ .' ()');
         $planning->update($request->all());
-        return $planning->toJson();
+        $planning->toJson();
     }
 
     /**
